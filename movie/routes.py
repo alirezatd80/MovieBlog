398514a22194f2ea1f70fa33d885f58fdaa17118
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-04-20 18:39:14 +0330

    linked pages

--- movie/routes.py
+++ movie/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, url_for
 from movie import app
 
 @app.route('/')
 def mainpage():
-    return render_template("adminpage/addmovie.html")
+    return render_template("app-landing.html")
+
+@app.route('/Movies')
+def mainmovies():
+    return render_template("movies.html")
