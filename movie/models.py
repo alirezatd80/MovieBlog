1f83ef6a505457adc3d2249ecd1dbe8034e973a8
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-04-24 13:46:42 +0330

    create movie model

--- movie/models.py
+++ movie/models.py
@@ -57,3 +57,67 @@ class Admin:
         order.close()
         connection.close()    
 
+class Movie:
+    def __init__(self,name , country,time,rate,year,gener,summery,photo_url,adminid) :
+        self.name = name
+        self.country = country
+        self.time = time
+        self.rate = rate
+        self.year = year
+        self.gener = gener
+        self.summery = summery
+        self.photo_url = photo_url
+        self.adminid = adminid
+        
+    def addmovie(self):
+         connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1111",
+            database="moviebank"
+        )
+         
+         query = "INSERT INTO movies (name, country, time , rate, year, gener, summery , photo_url , adminid) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+         values = (f"{self.name}", f"{self.country}", f"{self.time} " , f"{self.rate} " , f"{self.year} " ,f"{self.gener} " ,f"{self.summery} " ,f"{self.photo_url} " ,f"{self.adminid} ")
+         
+         order = connection.cursor()
+    
+         order.execute(query,values)
+         connection.commit()
+         print("add success")
+         order.close()
+         connection.close()
+        
+    def delmovie(self):
+        connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "1111",
+        database = "moviebank")
+
+        query = f"DELETE FROM moviebank.movies WHERE name='{self.name}'"
+
+        order = connection.cursor()
+        order.execute(query)
+        connection.commit()
+        print("del success")
+        order.close()
+        connection.close()    
+    
+    def editemovie(self,newname , newcountry,newtime,newrate,newyear,newgener,newsummery,newphoto_url ):
+        connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "1111",
+        database = "moviebank")
+
+        query = f"UPDATE moviebank.movies SET name = '{newname}', country = '{newcountry}',time = '{newtime}',rate = '{newrate}',year = '{newyear}',gener = '{newgener}',summery = '{newsummery}',photo_url = '{newphoto_url}' WHERE name = '{self.name}'"
+
+        order = connection.cursor()
+        order.execute(query)
+        connection.commit()
+        print("edit success")
+        order.close()
+        connection.close() 
+        
+
